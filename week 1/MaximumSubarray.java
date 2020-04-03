06f7a5099fd95cc55b6c9c882b790bd1c1557d68
Saikou Ceesay <saikouceesay@Saikous-MacBook-Pro.local>
2020-04-03 14:52:15 -0400

    description added

--- week 1/MaximumSubarray.java
+++ week 1/MaximumSubarray.java
@@ -1,6 +1,18 @@
+/**
+ * Given an integer array nums, find the contiguous subarray (containing at
+ * least one number) which has the largest sum and return its sum.
+ * 
+ * Example:
+ * 
+ * Input: [-2,1,-3,4,-1,2,1,-5,4], Output: 6 Explanation: [4,-1,2,1] has the
+ * largest sum = 6. Follow up:
+ * 
+ * If you have figured out the O(n) solution, try coding another solution using
+ * the divide and conquer approach, which is more subtle.
+ **/
 
 public class MaximumSubarray {
-    public int maxSubArray(int[] nums) {
+    public static int maxSubArray(int[] nums) {
         // use Kadane's Algorithm
         if (nums.length == 1 || nums == null)
             return nums[0];
@@ -13,5 +25,10 @@ public class MaximumSubarray {
             globalSum = Math.max(localSum, globalSum);
         }
         return globalSum;
+    }
+
+    public static void main(String arg[]) {
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println(maxSubArray(arr));
     }
 }
